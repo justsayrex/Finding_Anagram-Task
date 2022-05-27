d29e1f6ae4764a062552ecc1f16cd899788da736
justsayrex <justsayrex@gmail.com>
2022-05-27 12:13:52 +0100

    Create main.py
    Finding_anagram solution

---
+++ main.py
@@ -0,0 +1,13 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+
+    def find_anagram(word, anagram):
+          find_anagram("shoe", "hoes") --> True
+          find_anagram("soul", "lose") --> False
+
+          return True
+          else False
